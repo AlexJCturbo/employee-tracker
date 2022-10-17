08da7b139d6b5d8e02dd449fef5ae3153753faac
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-10-17 11:32:31 -0500

    added routes to view tables data and add entries

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 router.use(require('./departmentRoutes'));
 router.use(require('./roleRoutes'));
-router.use(require('./employeeRputes'));
+router.use(require('./employeeRoutes'));
 
 module.exports = router;
