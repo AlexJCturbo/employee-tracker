605141d3d6d46d47057df6c028381d3133a2ca78
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-10-16 23:38:47 -0500

    initial set up

---
+++ db/seeds.sql
@@ -0,0 +1,34 @@
+INSERT INTO department (name)
+VALUES
+('Engineering'),
+('Finance'),
+('Sales'),
+('Legal'),
+('Marketing');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Operations Manager', 210000, 1),
+('Account Manager', 155000, 2),
+('Account', 110000, 2),
+('Lead Engineer', 150000, 1),
+('Software Engineer', 115000, 1),
+('Sales Lead', 100000, 3),
+('Salesperson', 75000, 3),
+('Lawyer', 150000, 4),
+('Marketing Lead', 150000, 5),
+('Marketing analyst', 90000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Thomas', 'Brown', 1, NULL),
+('Katya', 'Andrews', 2, NULL),
+('Larissa', 'Fernandez', 3, 2),
+('Egemen', 'Erol', 4, NULL),
+('Alex', 'Castillo', 5, 4),
+('Victor', 'Chan', 5, 4),
+('Emma', 'Lourd', 6, 1),
+('Robert', 'Medvedev', 7, 6),
+('Jacob', 'Murphy', 8, NULL),
+('Sophia', 'Smorodina', 9, 1),
+('Mila', 'Cameron', 10, 9);
