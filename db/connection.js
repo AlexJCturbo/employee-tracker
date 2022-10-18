0916d193185c28a28ee049b7fe6497dcb4f67ff4
Alejandro Jaimes <108366893+AlexJCturbo@users.noreply.github.com>
2022-10-18 10:51:00 -0500

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection(
   {
   host: 'localhost',
-  user: 'root',
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
+  database: 'employee_tracker'
   },
-  console.log('Connected to the employee tracker database.')
+  console.log('You are now connected to the employee tracker database.')
 );
 
 module.exports = db;
